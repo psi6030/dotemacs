582bb600579ca1e4250f77419ffe1477212bfa55
Simon Swain <psurkit@gmail.com>
2012-05-27 22:25:17 +1000

    Cleaned up

--- init.el
+++ init.el
@@ -7,11 +7,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-js
-                                  slime slime-repl
-                      php-mode
-                      clojure-mode clojure-test-mode
-                      exapnd-region)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-js slime slime-repl php-mode clojure-mode clojure-test-mode exapnd-region)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -21,52 +17,34 @@
 (mouse-avoidance-mode 'exile)
 (delete-selection-mode 1)
 
-(setq inferior-lisp-program "/usr/bin/clisp")
-(add-to-list 'load-path "~/.emacs.d/managed-packages/slime-from-cvs/slime")  ; SLIME from CVS directory
-(require 'slime)
-(slime-setup)
-
-;; (defun slime-common-lisp ()
-;;   (interactive)
-;;   (setq inferior-lisp-program "/usr/bin/clisp") ; Common Lisp impl
-;;   (require 'slime)
-;;   (slime-setup '(slime-repl))
-;;   (slime))
-
-
-;;(set-default-font "-*-Courier New-normal-r-*-*-12-112-96-96-c-*-iso8859-1")
-
-;;(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
-
-(global-set-key [f5] 'call-last-kbd-macro)
-
-(set-frame-position (selected-frame) 0 0) 
 
 (load-theme ' tango-dark)
 (set-background-color "black")
 
 (setq 
-  ido-case-fold  t                 ; be case-insensitive
-  ido-enable-last-directory-history t ; remember last used dirs
-  ido-max-work-directory-list 30   ; should be enough
-  ido-max-work-file-list      50   ; remember many
-  ido-use-filename-at-point nil    ; don't use filename at point (annoying)
-  ido-use-url-at-point nil         ; don't use url at point (annoying)
-  ido-confirm-unique-completion t) ; wait for RET, even with unique completion
+ ido-case-fold  t                 ; be case-insensitive
+ ido-enable-last-directory-history t ; remember last used dirs
+ ido-max-work-directory-list 30   ; should be enough
+ ido-max-work-file-list      50   ; remember many
+ ido-use-filename-at-point nil    ; don't use filename at point (annoying)
+ ido-use-url-at-point nil         ; don't use url at point (annoying)
+ ido-confirm-unique-completion t) ; wait for RET, even with unique completion
 
 (setq column-number-mode 1)
 (setq flymake-gui-warnings-enabled nil)
 
 (defun rt-do-line-comments ()
-(setq comment-start "// ")
-(setq comment-end ""))
+  (setq comment-start "// ")
+  (setq comment-end ""))
 (add-hook 'c-mode-hook 'rt-do-line-comments)
 
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 
+;; windows font
+;;(set-default-font "-*-Courier New-normal-r-*-*-12-112-96-96-c-*-iso8859-1")
+
 (set-default-font "-unknown-Ubuntu Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
+
+(set-frame-position (selected-frame) 0 0) 
